972c358ba4690528691e335dca3e130c6bda55eb
DIONECAR <poxyi>
2023-04-22 19:56:46 +0700

    Ui moce

--- calculator/mainwindow.cpp
+++ calculator/mainwindow.cpp
@@ -1,15 +1,30 @@
 #include "mainwindow.h"
+
+#include "depcalc.h"
+#include "graph.h"
+#include "loancalc.h"
 #include "ui_mainwindow.h"
-
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::on_Graph_clicked() {
+  graph.setModal(true);
+  graph.setWindowTitle("ГРАФИК");
+  graph.exec();
 }
 
+void MainWindow::on_Credit_clicked() {
+  loanCalc.setModal(true);
+  loanCalc.setWindowTitle("КРЕДИТНЫЙ");
+  loanCalc.exec();
+}
+
+void MainWindow::on_Deposit_clicked() {
+  depCalc.setModal(true);
+  depCalc.setWindowTitle("ДЕПОЗИТНЫЙ");
+  depCalc.exec();
+}
--- calculator/mainwindow.h
+++ calculator/mainwindow.h
@@ -3,19 +3,34 @@
 
 #include <QMainWindow>
 
+#include "depcalc.h"
+#include "graph.h"
+#include "loancalc.h"
+
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+ public:
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+ private slots:
+  void on_Graph_clicked();
+
+  void on_Credit_clicked();
+
+  void on_Deposit_clicked();
+
+ private:
+  Ui::MainWindow *ui;
+  Graph graph;
+  DepCalc depCalc;
+  LoanCalc loanCalc;
 };
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
