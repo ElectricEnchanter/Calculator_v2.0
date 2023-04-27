711337cd6e4cac37b7c798a8ef238c39bac24853
DIONECAR <poxyi>
2023-04-27 17:46:18 +0700

    DepCalc

--- src/calculator/depcalc.cpp
+++ src/calculator/depcalc.cpp
@@ -1,14 +1,15 @@
 #include "depcalc.h"
+
 #include "ui_depcalc.h"
 
-DepCalc::DepCalc(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::DepCalc)
-{
-    ui->setupUi(this);
+DepCalc::DepCalc(QWidget *parent) : QDialog(parent), ui(new Ui::DepCalc) {
+  ui->setupUi(this);
+  ui->calendarWidget->hide();
+
+  ui->depAmountInput->setValidator(new QIntValidator(0, 10000000, this));
+  ui->interRateInput->setValidator(new QIntValidator(0, 10, this));
 }
 
-DepCalc::~DepCalc()
-{
-    delete ui;
-}
+DepCalc::~DepCalc() { delete ui; }
+
+void DepCalc::on_closeButton_clicked() { this->close(); }
